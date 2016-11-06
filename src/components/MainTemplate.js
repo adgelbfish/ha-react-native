281e25e0143b0b9ff6ca54b07d8d432f04ada157
Avraham David Gelbfish <adg@adgelb.fish>
2016-11-06 01:59:09 -0400

    pulled an unnecessary constructor function

--- src/components/MainTemplate.js
+++ src/components/MainTemplate.js
@@ -1,14 +1,10 @@
 'use strict';
 
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import {Container, Content, Header, Button, Icon, Title} from 'native-base';
 
 
 export default class MainTemplate extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         const {url} = this.props;
 
